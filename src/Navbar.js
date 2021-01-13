d41a289e234567c191bb8157e4f69cdb903b5ebc
Florence <monnaie.florence@gmail.com>
2021-01-13 11:22:33 +0100

    fin exo 3

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar () {
+    return (
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CONTACT</li>
+                <li>PORTFOLIO</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
