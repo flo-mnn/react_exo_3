55dac085a4bc00050772c5d1f840185896dde6b9
Florence <monnaie.florence@gmail.com>
2021-01-13 11:13:32 +0100

    modification du component App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import img from "./norway.jpg"
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Exercice 3</h1>
+      <img src={img} alt="norway"/>
     </div>
   );
 }
